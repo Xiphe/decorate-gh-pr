a256d9210793180708cde1286f02a935b0dfde5e
Xiphe <github@xiphe.net>
2020-03-02 10:06:41 +0100

    feat: add semantic-release plugin

---
+++ on-release.js
@@ -0,0 +1,37 @@
+const getEnvCi = require('env-ci');
+const template = require('lodash.template');
+const decorateGhPr = require('./index');
+
+function resolveComment(comment, args) {
+  switch (typeof comment) {
+    case 'string':
+      return template(comment)(args);
+    case 'function':
+      return comment(args);
+    default:
+      return `released ${args.name}`;
+  }
+}
+
+module.exports = {
+  async publish(pluginConfig, { cwd, env, logger, nextRelease }) {
+    const envCi = getEnvCi({ cwd, env });
+
+    if (!envCi.isPr) {
+      logger.log('Skipping PR decoration since no PR was found');
+      return;
+    }
+
+    const { pr, comment } = await decorateGhPr({
+      comment: resolveComment(pluginConfig.comment, {
+        ...nextRelease,
+        date: new Date(),
+      }),
+      prepend: pluginConfig.prepend,
+      id: pluginConfig.id,
+      env: envCi,
+    });
+
+    logger.success(`Updated ${pr}: ${comment}`);
+  },
+};
