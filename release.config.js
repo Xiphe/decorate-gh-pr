00d72494aa6405f58be5e067a36394af5bbe974b
Xiphe <github@xiphe.net>
2020-10-12 23:44:28 +0200

    chore: update semantic-release branches config

--- release.config.js
+++ release.config.js
@@ -2,6 +2,14 @@
 const isStable = !process.env.PRE_RELEASE;
 
 module.exports = {
+  branches: [
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'main',
+    'next',
+    'next-major',
+    {name: 'beta', prerelease: true},
+    {name: 'alpha', prerelease: true}
+  ],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
